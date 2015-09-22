9c1905b829cc96ab831fb9609373644d809ddb93
Chris Loewer <chris.loewer@gmail.com>
2015-09-21 23:39:53 -0500

    Add db and homepage inflates template.

--- blog.rb
+++ blog.rb
@@ -4,6 +4,26 @@ require 'json'
 
 set :port, 12008
 
+# DATABASE
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/posts.db")
+
+class Post
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, Text, :required => true, :lazy =>false
+  property :author, Text, :lazy =>false
+  property :content, Text, :required => true
+  property :preview, Text, :required => true
+  property :time, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
 get '/' do
   erb :home
+end
+
+get '/api/posts' do
+  @posts = Post.all
+  @posts.to_json()
 end
--- public/js/templates.js
+++ public/js/templates.js
@@ -9,4 +9,21 @@ templates['header'] = template({"compiler":[7,">= 4.0.0"],"main":function(contai
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h1>\n";
 },"useData":true});
+templates['post_previews'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=container.escapeExpression;
+
+  return "  <a href=\""
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "\">\n    <div class=\"post-container\">\n      <div class=\"post-title\">"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</div>\n      <div class=\"post-author\">by "
+    + alias3(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"author","hash":{},"data":data}) : helper)))
+    + "</div>\n      <div class=\"post-preview\">\n        "
+    + ((stack1 = ((helper = (helper = helpers.preview || (depth0 != null ? depth0.preview : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"preview","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "\n      </div>\n    </div>\n  </a>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 })();
