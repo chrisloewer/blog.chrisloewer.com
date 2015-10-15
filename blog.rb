22c1138c8755d9ee1698f4ccc770a66aee538419
Chris Loewer <chris.loewer@gmail.com>
2015-10-14 19:09:16 -0500

    Change routing path to start under /blog/ directory.

--- blog.rb
+++ blog.rb
@@ -22,12 +22,16 @@ end
 DataMapper.finalize.auto_upgrade!
 
 # ROUTING
-get '/' do
+
+# NOTE - blog located under '/blog/' subdirectory
+#        All paths must start with /blog/
+
+get '/blog/' do
   @tab_title = 'Techster'
   erb :home
 end
 
-get '/:path' do |p|
+get '/blog/:path' do |p|
   @tab_title= p.split('-').map(&:capitalize).join(' ')
 
   @path = p
@@ -35,12 +39,12 @@ get '/:path' do |p|
 end
 
 # APIs
-get '/api/posts' do
+get '/blog/api/posts' do
   @posts = Post.all
   @posts.to_json()
 end
 
-get '/api/posts/:path' do |p|
+get '/blog/api/posts/:path' do |p|
   @post = Post.first(:path => p)
   @post.to_json();
 end
