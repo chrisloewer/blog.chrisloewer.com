43d7750ca4b695a11f7a84a01eae3ca82b6776aa
Chris Loewer <chris.loewer@gmail.com>
2015-10-25 20:42:21 -0500

    Change base directory from "/blog/" to "/" due to switch to subdomain.

--- blog.rb
+++ blog.rb
@@ -26,15 +26,12 @@ DataMapper.finalize.auto_upgrade!
 
 # ROUTING
 
-# NOTE - blog located under '/blog/' subdirectory
-#        All paths must start with /blog/
-
-get '/blog/' do
+get '/' do
   @tab_title = 'Techster'
   erb :home
 end
 
-get '/blog/:path' do |p|
+get '/:path' do |p|
   @tab_title= p.split('-').map(&:capitalize).join(' ')
 
   @path = p
@@ -42,12 +39,12 @@ get '/blog/:path' do |p|
 end
 
 # APIs
-get '/blog/api/posts' do
+get '/api/posts' do
   @posts = Post.all
   @posts.to_json()
 end
 
-get '/blog/api/posts/:path' do |p|
+get '/api/posts/:path' do |p|
   @post = Post.first(:path => p)
   @post.to_json();
 end
@@ -59,6 +56,7 @@ end
 configure do
   mime_type :js, 'application/javascript'
 end
+
 configure do
   mime_type :svg, 'image/svg+xml'
 end
@@ -67,20 +65,19 @@ end
 def js(f)
   File.read(File.join('public/js', "#{f.to_s}"))
 end
+
 def svg(f)
-  puts 'svg'
   File.read(File.join('public/img', "#{f.to_s}"))
 end
 
 # paths to catch static files
-get '/blog/js/*' do |file|
+get '/js/*' do |file|
   content_type :js
   js file
 end
 
-get '/blog/img/*.*' do |file, ext|
+get '/img/*.*' do |file, ext|
   if ext == 'svg'
-    puts 'SVG'
     content_type :svg
     svg(file + '.' + ext)
   end
